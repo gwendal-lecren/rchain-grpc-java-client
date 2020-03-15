b876dda08716976c3d974ecb9a176fab1c1898cb
Gwendal Le Cren <gwendal.le-cren@revevol.eu>
2020-03-15 16:57:34 +0100

    Init rchain grpc java client

---
+++ src/main/java/coop/rchain/AbstractClient.java
@@ -0,0 +1,40 @@
+package coop.rchain;
+
+import io.grpc.stub.AbstractAsyncStub;
+import io.grpc.stub.AbstractBlockingStub;
+
+public class AbstractClient<T extends AbstractBlockingStub, U extends AbstractAsyncStub> {
+
+    protected final T blockingStub;
+    protected final U asyncStub;
+
+    protected AbstractClient(Builder builder) {
+        blockingStub = (T) builder.blockingStub;
+        asyncStub = (U) builder.asyncStub;
+    }
+
+    public abstract static class Builder<T extends AbstractBlockingStub, U extends AbstractAsyncStub> {
+        T blockingStub;
+        U asyncStub;
+
+        protected Builder(T blockingStub, U asyncStub) {
+            this.blockingStub = blockingStub;
+            this.asyncStub = asyncStub;
+        }
+
+        public abstract AbstractClient build();
+
+        public Builder setBlockingStub(T blockingStub) {
+            this.blockingStub = (T) blockingStub;
+            return this;
+        }
+
+        public Builder setAsyncStub(U asyncStub) {
+            this.asyncStub = (U) asyncStub;
+            return this;
+        }
+        
+    }
+
+
+}
---
+++ src/main/java/coop/rchain/casper/protocol/propose/v1/ProposeService.java
@@ -0,0 +1,49 @@
+package coop.rchain.casper.protocol.propose.v1;
+
+import coop.rchain.AbstractClient;
+import coop.rchain.casper.protocol.ProposeServiceCommon;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
+
+public class ProposeService extends AbstractClient {
+
+    ProposeService(Builder builder) {
+        super(builder);
+    }
+
+    public ProposeService(Channel channel) {
+        this(new Builder(ProposeServiceGrpc.newBlockingStub(channel), ProposeServiceGrpc.newStub(channel)));
+    }
+
+    public ProposeService(String target) {
+        this(ManagedChannelBuilder.forTarget(target).build());
+    }
+
+    public static final class Builder extends AbstractClient.Builder<ProposeServiceGrpc.ProposeServiceBlockingStub,
+            ProposeServiceGrpc.ProposeServiceStub> {
+        public Builder(ProposeServiceGrpc.ProposeServiceBlockingStub blockingStub, ProposeServiceGrpc.ProposeServiceStub asyncStub) {
+            super(blockingStub, asyncStub);
+        }
+
+        @Override
+        public AbstractClient build() {
+            return new ProposeService(this);
+        }
+
+        @Override
+        public Builder setBlockingStub(ProposeServiceGrpc.ProposeServiceBlockingStub blockingStub) {
+            return (Builder) super.setBlockingStub(blockingStub);
+        }
+
+        @Override
+        public Builder setAsyncStub(ProposeServiceGrpc.ProposeServiceStub asyncStub) {
+            return (Builder) super.setAsyncStub(asyncStub);
+        }
+    }
+
+
+    public ProposeServiceV1.ProposeResponse propose(ProposeServiceCommon.PrintUnmatchedSendsQuery request) {
+        return ((ProposeServiceGrpc.ProposeServiceBlockingStub) this.blockingStub).propose(request);
+    }
+
+}
